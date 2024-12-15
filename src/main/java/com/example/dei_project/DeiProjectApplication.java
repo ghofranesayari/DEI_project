678187c2b9dbb85e6164ebbb65f80e1c85a829e4
unknown <ghofranesayari.ghofrane162001@gmail.com>
2024-12-15 07:26:12 +0100

    m1

---
+++ src/main/java/com/example/dei_project/DeiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dei_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dei_project/DeiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dei_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
